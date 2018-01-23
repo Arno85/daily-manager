ef61450ee4004ec62ff4e85e6c94f231cc80a783
arno <arnaud.martin85@gmail.com>
2018-01-22 19:51:26 -0500

    angular cli updated + fix bug sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 
+/*
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'projects',      component: ProjectsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: HomeComponent }
+];*/
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    //RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
