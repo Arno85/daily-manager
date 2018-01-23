55b0d05252d4b1496ad2ef51e580c1c1be50e90e
arno <arnaud.martin85@gmail.com>
2018-01-22 22:29:42 -0500

    add list item + style shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,9 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  items: any = [
+    {
+      id: 1,
+      name: 'Concombre',
+      quantity: 3
+    },
+    {
+      id: 2,
+      name: 'Tomates Cerises',
+      quantity: 1
+    },
+    {
+      id: 3,
+      name: 'Pain',
+      quantity: 1,
+      specs: 'Saint Méthode'
+    }
+  ];
 
   constructor() { }
 
